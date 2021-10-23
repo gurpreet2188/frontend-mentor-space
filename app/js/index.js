0a594892a79b567890ef31d48e2d6e3a321c6093
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2021-10-23 17:04:21 +0800

    Added Crew Page-mobile

--- app/js/index.js
+++ app/js/index.js
@@ -26,7 +26,7 @@ function panel() {
     dynamicArea.innerHTML = bodyText;
     body.className = "";
     body.classList.add("background");
-    if(!window.matchMedia("(max-width: 64rem)")) {
+    if (!window.matchMedia("(max-width: 64rem)")) {
       navpanel.style.visibility = "hidden";
     } else {
       navpanel.style.visibility = "visible";
@@ -37,7 +37,17 @@ function panel() {
   destination.addEventListener("click", (e) => {
     dynamicArea.innerHTML = "";
     destinationArea();
-    if(!window.matchMedia("(max-width: 64rem)")) {
+    if (!window.matchMedia("(max-width: 64rem)")) {
+      navpanel.style.visibility = "hidden";
+    } else {
+      navpanel.style.visibility = "visible";
+    }
+  });
+
+  crew.addEventListener("click", (e) => {
+    dynamicArea.innerHTML = "";
+    crewArea();
+    if (!window.matchMedia("(max-width: 64rem)")) {
       navpanel.style.visibility = "hidden";
     } else {
       navpanel.style.visibility = "visible";
@@ -66,6 +76,19 @@ function loadData(type, image, title, body, km, time, i) {
           km.appendChild(avgDist);
           let est = aText(`${data.destinations[i].travel}`);
           time.appendChild(est);
+          break;
+
+        case "crew":
+          let cName = aText(`${data.crew[i].name}`);
+          title.appendChild(cName);
+          let cPara = aText(`${data.crew[i].bio}`);
+          body.appendChild(cPara);
+          image.style.backgroundImage = `url('${data.crew[i].images.webp}')`;
+          if (i === 0) {
+            image.style.marginLeft = "26%";
+          }
+          let role = aText(`${data.crew[i].role}`);
+          time.appendChild(role);
       }
     });
 }
@@ -74,6 +97,12 @@ function removeData(title, bodyText, km, time) {
   title.innerHTML = "";
   bodyText.innerHTML = "";
   km.innerHTML = "";
+  time.innerHTML = "";
+}
+
+function removeCrew(title, bodyText, time) {
+  title.innerHTML = "";
+  bodyText.innerHTML = "";
   time.innerHTML = "";
 }
 
@@ -166,7 +195,7 @@ function destinationArea() {
   );
 
   // button handling
-  buttonHandle(
+  buttonHandle('dest',
     clsNames,
     moon,
     pageLayout.image,
@@ -176,7 +205,7 @@ function destinationArea() {
     time,
     0
   );
-  buttonHandle(
+  buttonHandle('dest',
     clsNames,
     mars,
     pageLayout.image,
@@ -186,7 +215,7 @@ function destinationArea() {
     time,
     1
   );
-  buttonHandle(
+  buttonHandle('dest',
     clsNames,
     europa,
     pageLayout.image,
@@ -196,7 +225,7 @@ function destinationArea() {
     time,
     2
   );
-  buttonHandle(
+  buttonHandle('dest',
     clsNames,
     titan,
     pageLayout.image,
@@ -208,22 +237,125 @@ function destinationArea() {
   );
 }
 
-
 function crewArea() {
-    //header
-    //image
-    //divider
-    //
+  let pageLayout = new PageLayout();
+
+  body.className = "";
+  body.classList.add("crew-bg");
+
+  const crewDiv = document.createElement("div");
+  crewDiv.classList.add("main");
+  dynamicArea.appendChild(crewDiv);
+
+  //header
+  pageLayout.setHeader("02 MEET YOUR CREW", crewDiv, "main-header");
+  //image
+  pageLayout.setImage("main-image-crew", crewDiv);
+  //divider
+  const divider = document.createElement("span");
+  divider.classList.add("main-divider-crew");
+  crewDiv.appendChild(divider);
+  // slider
+  const sliderDiv = document.createElement("div");
+  crewDiv.appendChild(sliderDiv);
+  sliderDiv.classList.add("main-slider");
+  const sc0 = document.createElement("a");
+  const sc1 = document.createElement("a");
+  const sc2 = document.createElement("a");
+  const sc3 = document.createElement("a");
+  const scArr = [sc0, sc1, sc2, sc3];
+
+  for (i in scArr) {
+    scArr[i].href = "#";
+    sliderDiv.appendChild(scArr[i]);
+    scArr[i].classList.add("main-slider-circle");
+    if(scArr[i] !== sc0){
+      scArr[i].classList.add("inactive");
+    }
+  }
+
+  //subhead
+  pageLayout.setSubhead("main-subhead", crewDiv);
+  //title
+  pageLayout.setTitle("main-title-crew", crewDiv);
+  //para
+  pageLayout.setPara("main-body", crewDiv);
+
+  //set dfault
+  
+  loadData(
+    "crew",
+    pageLayout.image,
+    pageLayout.title,
+    pageLayout.para,
+    null,
+    pageLayout.subhead,
+    0
+  );
+  //sliders actions
+
+  buttonHandle(
+    "crew",
+    scArr,
+    sc0,
+    pageLayout.image,
+    pageLayout.title,
+    pageLayout.para,
+    null,
+    pageLayout.subhead,
+    0
+  );
+  buttonHandle(
+    "crew",
+    scArr,
+    sc1,
+    pageLayout.image,
+    pageLayout.title,
+    pageLayout.para,
+    null,
+    pageLayout.subhead,
+    1
+  );
+  buttonHandle(
+    "crew",
+    scArr,
+    sc2,
+    pageLayout.image,
+    pageLayout.title,
+    pageLayout.para,
+    null,
+    pageLayout.subhead,
+    2
+  );
+  buttonHandle(
+    "crew",
+    scArr,
+    sc3,
+    pageLayout.image,
+    pageLayout.title,
+    pageLayout.para,
+    null,
+    pageLayout.subhead,
+    3
+  );
 }
-
-
 
 function aText(tx) {
   let aT = document.createTextNode(`${tx}`);
   return aT;
 }
 
-function buttonHandle(clsNames, btn, image, title, bodyText, km, time, i) {
+function buttonHandle(
+  type,
+  clsNames,
+  btn,
+  image,
+  title,
+  bodyText,
+  km,
+  time,
+  i
+) {
   btn.addEventListener("click", (e) => {
     for (c in clsNames) {
       if (!clsNames[c].classList.contains("inactive")) {
@@ -231,8 +363,17 @@ function buttonHandle(clsNames, btn, image, title, bodyText, km, time, i) {
       }
     }
     btn.classList.remove("inactive");
-    setTimeout(removeData(title, bodyText, km, time), 100);
-    setTimeout(loadData("dest", image, title, bodyText, km, time, i), 300);
+    
+    switch (type) {
+      case "dest":
+        setTimeout(removeData(title, bodyText, km, time), 100);
+        setTimeout(loadData("dest", image, title, bodyText, km, time, i), 300);
+        break
+      case "crew":
+        setTimeout(removeCrew(title, bodyText, time), 100);
+        setTimeout(loadData("crew", image, title, bodyText, km, time, i), 300);
+        break
+    }
   });
 }
 
@@ -240,7 +381,7 @@ class PageLayout {
   constructor(header, image, subhead, title, para) {
     this.header = document.createElement("p");
     this.image = document.createElement("div");
-    this.subhead = subhead;
+    this.subhead = document.createElement("h3");
     this.title = document.createElement("h1");
     this.para = document.createElement("p");
   }
@@ -255,6 +396,11 @@ class PageLayout {
   setImage(cls, baseDiv) {
     this.image.classList.add(`${cls}`);
     baseDiv.appendChild(this.image);
+  }
+
+  setSubhead(cls, baseDiv) {
+    this.subhead.classList.add(`${cls}`);
+    baseDiv.appendChild(this.subhead);
   }
 
   setTitle(cls, baseDiv) {
