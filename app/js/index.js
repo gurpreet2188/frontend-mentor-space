96c3c9dd4b50a9c828a54d97409ee43177f62e8d
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2021-10-23 20:42:25 +0800

    Added tech page for mobile

--- app/js/index.js
+++ app/js/index.js
@@ -53,7 +53,20 @@ function panel() {
       navpanel.style.visibility = "visible";
     }
   });
+
+  technology.addEventListener("click", (e) => {
+    dynamicArea.innerHTML = "";
+    techArea();
+    if (!window.matchMedia("(max-width: 64rem)")) {
+      navpanel.style.visibility = "hidden";
+    } else {
+      navpanel.style.visibility = "visible";
+    }
+  });
 }
+
+
+
 
 panel();
 
@@ -67,7 +80,7 @@ function loadData(type, image, title, body, km, time, i) {
     .then((data) => {
       switch (type) {
         case "dest":
-          image.style.backgroundImage = `url('${data.destinations[i].images.webp}')`;
+          image.style.backgroundImage = `url('${data.destinations[i].images.webp}')`
           let tText = aText(`${data.destinations[i].name}`);
           title.appendChild(tText);
           let bText = aText(`${data.destinations[i].description}`);
@@ -83,12 +96,21 @@ function loadData(type, image, title, body, km, time, i) {
           title.appendChild(cName);
           let cPara = aText(`${data.crew[i].bio}`);
           body.appendChild(cPara);
-          image.style.backgroundImage = `url('${data.crew[i].images.webp}')`;
+          image.style.backgroundImage = `url('${data.crew[i].images.webp}')`
           if (i === 0) {
             image.style.marginLeft = "26%";
           }
           let role = aText(`${data.crew[i].role}`);
           time.appendChild(role);
+          break;
+
+      case "tech":
+          let tName = aText(`${data.technology[i].name}`);
+          title.appendChild(tName);
+          let tPara = aText(`${data.technology[i].description}`);
+          body.appendChild(tPara);
+          image.style.backgroundImage = `url('${data.technology[i].images.landscape}')`
+          break;
       }
     });
 }
@@ -104,6 +126,11 @@ function removeCrew(title, bodyText, time) {
   title.innerHTML = "";
   bodyText.innerHTML = "";
   time.innerHTML = "";
+}
+
+function removeTech(title, bodyText) {
+  title.innerHTML = "";
+  bodyText.innerHTML = "";
 }
 
 function destinationArea() {
@@ -340,6 +367,71 @@ function crewArea() {
   );
 }
 
+function techArea() {
+  let pageLayout = new PageLayout();
+  body.className = "";
+  body.classList.add("tech-bg");
+  const techDiv = document.createElement("div");
+  techDiv.classList.add("main");
+  dynamicArea.appendChild(techDiv);
+
+  //header
+  pageLayout.setHeader("03 SPACE LAUNCH 101", techDiv,  "main-header")
+
+  //image
+  pageLayout.setImage('main-image-tech', techDiv)
+
+  //buttons
+  const btnDiv = document.createElement('div')
+  techDiv.appendChild(btnDiv)
+  btnDiv.classList.add('main-tech')
+
+  const btn0 = document.createElement('a')
+  const btn1 = document.createElement('a')
+  const btn2 = document.createElement('a')
+
+  const btnArr = [btn0, btn1, btn2]
+
+  for (i in btnArr) {
+    btnArr[i].href = "#";
+    btnDiv.appendChild(btnArr[i]);
+    btnArr[i].classList.add("main-tech-btn");
+    if(btnArr[i] !== btn0) {
+      btnArr[i].classList.add('btn-off')
+    } 
+  }
+  btn0.appendChild(aText('1'))
+  btn1.appendChild(aText('2'))
+  btn2.appendChild(aText('3'))
+
+  // text
+  const txt = document.createElement('p')
+  const pText = document.createTextNode('THE TERMINOLOGY...')
+  txt.appendChild(pText)
+  techDiv.appendChild(txt)
+  txt.classList.add('main-subhead')
+  //title
+  pageLayout.setTitle('main-title-crew', techDiv)
+  //para
+  pageLayout.setPara('main-body', techDiv)
+
+  // set default
+  loadData(
+    "tech",
+    pageLayout.image,
+    pageLayout.title,
+    pageLayout.para,
+    null,
+    null,
+    0
+  );
+
+  //butons 
+  buttonHandle('tech', btnArr, btn0, pageLayout.image, pageLayout.title, pageLayout.para, null, null, 0)
+  buttonHandle('tech', btnArr, btn1, pageLayout.image, pageLayout.title, pageLayout.para, null, null, 1)
+  buttonHandle('tech', btnArr, btn2, pageLayout.image, pageLayout.title, pageLayout.para, null, null, 2)
+}
+
 function aText(tx) {
   let aT = document.createTextNode(`${tx}`);
   return aT;
@@ -357,21 +449,37 @@ function buttonHandle(
   i
 ) {
   btn.addEventListener("click", (e) => {
-    for (c in clsNames) {
-      if (!clsNames[c].classList.contains("inactive")) {
-        clsNames[c].classList.add("inactive");
-      }
-    }
-    btn.classList.remove("inactive");
     
     switch (type) {
       case "dest":
+        for (d in clsNames) {
+          if (!clsNames[d].classList.contains("inactive")) {
+            clsNames[d].classList.add("inactive");
+          }
+        }
+        btn.classList.remove("inactive");
         setTimeout(removeData(title, bodyText, km, time), 100);
         setTimeout(loadData("dest", image, title, bodyText, km, time, i), 300);
         break
       case "crew":
+        for (c in clsNames) {
+          if (!clsNames[c].classList.contains("inactive")) {
+            clsNames[c].classList.add("inactive");
+          }
+        }
+        btn.classList.remove("inactive");
         setTimeout(removeCrew(title, bodyText, time), 100);
         setTimeout(loadData("crew", image, title, bodyText, km, time, i), 300);
+        break
+        case "tech":
+          for (t in clsNames) {
+            if (!clsNames[t].classList.contains("btn-off")) {
+              clsNames[t].classList.add("btn-off");
+            }
+          }
+          btn.classList.remove("btn-off")
+        setTimeout(removeTech(title, bodyText), 100);
+        setTimeout(loadData("tech", image, title, bodyText, km, time, i), 300);
         break
     }
   });
@@ -386,11 +494,11 @@ class PageLayout {
     this.para = document.createElement("p");
   }
 
-  setHeader(txt, baseDiv, clsname) {
+  setHeader(txt, baseDiv, cls) {
     let text = document.createTextNode(`${txt}`);
     this.header.appendChild(text);
     baseDiv.appendChild(this.header);
-    this.header.classList.add(`${clsname}`);
+    this.header.classList.add(`${cls}`);
   }
 
   setImage(cls, baseDiv) {
